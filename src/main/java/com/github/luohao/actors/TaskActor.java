966b09903b7e8163b4f4df393996c7f78c67bf30
luohao <553569682@qq.com>
2016-10-31 15:43:24 +0800

    spring akka

---
+++ src/main/java/com/github/luohao/actors/TaskActor.java
@@ -0,0 +1,28 @@
+package com.github.luohao.actors;
+
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import com.github.luohao.beans.Task;
+import com.github.luohao.services.TaskDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class TaskActor extends UntypedActor {
+
+    private final LoggingAdapter log = Logging
+        .getLogger(getContext().system(), "TaskProcessor");
+
+    @Autowired
+    private TaskDAO taskDAO;
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+
+        Long result = taskDAO.createTask((Task) message);
+        log.debug("Created task {}", result);
+    }
+}
---
+++ src/main/java/com/github/luohao/mailbox/PriorityMailbox.java
@@ -0,0 +1,31 @@
+package com.github.luohao.mailbox;
+
+import akka.actor.ActorSystem;
+import akka.dispatch.PriorityGenerator;
+import akka.dispatch.UnboundedPriorityMailbox;
+import com.github.luohao.beans.Task;
+import com.typesafe.config.Config;
+
+/**
+ * Simple priority queue mapping the task priority to the mailbox priority.
+ */
+public class PriorityMailbox extends UnboundedPriorityMailbox {
+
+    public PriorityMailbox(ActorSystem.Settings settings, Config config) {
+
+        // Create a new PriorityGenerator, lower priority means more important
+        super(new PriorityGenerator() {
+
+            @Override
+            public int gen(Object message) {
+                if (message instanceof Task) {
+                    return ((Task) message).getPriority();
+                } else {
+                    // default
+                    return 100;
+                }
+            }
+        });
+
+    }
+}
